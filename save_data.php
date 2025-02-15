4b2ec20546624dd754c3bdc2f2134635129e3239
jakosyc <jakob.sycha@t-online.de>
2025-02-15 18:49:55 +0100

    Update save_data.php

--- save_data.php
+++ save_data.php
@@ -1,53 +1,57 @@
 <?php
-// PHPMailer und Exception einbinden (Pfad zu PHPMailer anpassen)
+// PHPMailer einbinden
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require 'vendor/autoload.php'; // Falls du Composer verwendest
-// Oder manuell: require 'Pfad/zu/PHPMailer/PHPMailerAutoload.php';
+// Autoload-Datei von PHPMailer einbinden
+require 'path/to/PHPMailer/src/Exception.php';
+require 'path/to/PHPMailer/src/PHPMailer.php';
+require 'path/to/PHPMailer/src/SMTP.php';
 
-// Formularverarbeitung
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    // Benutzereingaben sanitieren
-    $name = htmlspecialchars($_POST['name']);
-    $email = htmlspecialchars($_POST['email']);
-    $message = htmlspecialchars($_POST['message']);
+// Überprüfen, ob das Formular übermittelt wurde
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Sammeln der Formulardaten
+    $veranstaltungsname = isset($_POST['name']) ? $_POST['name'] : '';
+    $datetime = isset($_POST['datetime']) ? $_POST['datetime'] : '';
+    $artists = isset($_POST['artists']) ? $_POST['artists'] : '';
+    $location = isset($_POST['location']) ? $_POST['location'] : '';
+    $mood = isset($_POST['mood']) ? $_POST['mood'] : '';
+    $email = isset($_POST['email']) ? $_POST['email'] : '';
 
-    // Instanz von PHPMailer erstellen
+    // E-Mail-Inhalt erstellen
+    $email_content = "Veranstaltungsname: " . $veranstaltungsname . "\n";
+    $email_content .= "Uhrzeit & Datum: " . $datetime . "\n";
+    $email_content .= "Artists: " . $artists . "\n";
+    $email_content .= "Location & Adresse & Stadt: " . $location . "\n";
+    $email_content .= "Mood/Genre: " . $mood . "\n";
+    $email_content .= "Deine E-Mail Adresse: " . $email . "\n";
+
+    // Erstelle eine neue Instanz von PHPMailer
     $mail = new PHPMailer(true);
-    
+
     try {
-        // SMTP-Server konfigurieren
-        $mail->isSMTP();
-        $mail->Host = 'smtp.gmail.com'; // Ersetze mit deinem SMTP-Host (z.B. smtp.gmail.com für Gmail)
-        $mail->SMTPAuth = true;
-        $mail->Username = 'spuckerfeuer@gmail.com'; // Deine E-Mail-Adresse
-        $mail->Password = 'FSHooligans'; // Dein E-Mail-Passwort
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-        $mail->Port = 587; // SMTP-Port (587 für TLS, 465 für SSL)
+        // SMTP-Server-Einstellungen
+        $mail->isSMTP();                                           // Setze Mailer auf SMTP
+        $mail->Host = 'smtp.gmail.com';                             // Gmail SMTP-Server
+        $mail->SMTPAuth = true;                                     // Aktiviere SMTP-Authentifizierung
+        $mail->Username = 'spuckerfeuer@gmail.com';                  // Deine Gmail-Adresse
+        $mail->Password = 'FSHooligans';                           // Dein Gmail-Passwort (oder App-Passwort)
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Aktiviere TLS-Verschlüsselung
+        $mail->Port = 587;                                          // TCP-Port für TLS
 
         // Absender und Empfänger
-        $mail->setFrom($email, $name); // Der Absender ist die E-Mail-Adresse, die im Formular eingegeben wurde
-        $mail->addAddress('spuckerfeuer@gmail.com', 'Empfänger Name'); // Ziel-E-Mail-Adresse (Empfänger)
+        $mail->setFrom('spuckerfeuer@gmail.com', 'Veranstaltungssystem');  // Absender-Adresse
+        $mail->addAddress('spuckerfeuer@gmail.com', 'Empfänger Name');     // Empfänger-Adresse (auch deine Gmail-Adresse)
 
-        // E-Mail-Inhalt
-        $mail->isHTML(true);
-        $mail->Subject = 'Neue Nachricht vom Kontaktformular: ' . $name;
-        $mail->Body    = "<h3>Neue Nachricht:</h3>
-                          <strong>Name:</strong> $name<br>
-                          <strong>E-Mail:</strong> $email<br>
-                          <strong>Nachricht:</strong><br>" . nl2br($message);
+        // Betreff und Inhalt der E-Mail
+        $mail->Subject = 'Neue Veranstaltungseinreichung';
+        $mail->Body    = $email_content;
 
         // E-Mail senden
-        if ($mail->send()) {
-            echo 'Vielen Dank für Ihre Nachricht! Wir werden uns bald bei Ihnen melden.';
-        } else {
-            echo 'Es gab ein Problem beim Senden der E-Mail. Bitte versuchen Sie es später noch einmal.';
-        }
+        $mail->send();
+        echo 'Die E-Mail wurde erfolgreich gesendet.';
     } catch (Exception $e) {
-        echo "Die Nachricht konnte nicht gesendet werden. Fehler: {$mail->ErrorInfo}";
+        echo "Es gab ein Problem beim Senden der E-Mail. Fehler: {$mail->ErrorInfo}";
     }
-} else {
-    echo "Ungültige Anfrage.";
 }
 ?>
